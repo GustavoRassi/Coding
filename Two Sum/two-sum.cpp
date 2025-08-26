217fd76c52fae827c42fdd59cf4b5c71b2f74fcd
Gustavo A. Rassi <73265084+GustavoRassi@users.noreply.github.com>
2025-08-25 21:22:24 -0400

    Created two-sum.cpp

---
+++ Two Sum/two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer;
+
+        for (int i = 0; i < nums.size(); i++)
+            for (int x = i+1; x < nums.size(); x++)
+            {
+                if(nums[i] + nums[x] == target)
+                {
+                    answer.push_back(i);
+                    answer.push_back(x);
+                    return answer;
+                }
+            }
+        return answer;
+    }
+};
