c83c4ffcdfa5cdb034b1997445cd8eb963ebd730
Gustavo A. Rassi <gustavo.rassi@upr.edu>
2025-09-10 13:55:11 -0400

    added code and new problems

--- Codeforces/2132A.cpp
+++ Codeforces/2132A.cpp
@@ -1,21 +1,76 @@
 #include <iostream>
-#include <string>
+#include <deque>
 #include <fstream>
+#include <string>
 using namespace std;
 
-int main()
+int main() 
 {
-    string input = "efo";
-    ofstream outFile("testcase.txt");
+    ofstream outFile("2132A_testcase.txt");
+    outFile << "4\n";
     outFile << "2\n";
     outFile << "ot\n";
     outFile << "2\n";
     outFile << "ad\n";
-    outFile << "DV";
+    outFile << "DV\n";
+    outFile << "3\n";
+    outFile << "efo\n";
+    outFile << "7\n";
+    outFile << "rdcoecs\n";
+    outFile << "DVDVDVD\n";
+    outFile << "3\n";
+    outFile << "aca\n";
+    outFile << "4\n";
+    outFile << "bbaa\n";
+    outFile << "DVDV\n";
+    outFile << "3\n";
+    outFile << "biz\n";
+    outFile << "4\n";
+    outFile << "abon\n";
+    outFile << "VVDD";
     outFile.close();
-
-    ifstream inFile("testcase.txt");
+    
+    int testcases; // number of test cases
+    ifstream inFile("2132A_testcase.txt");
+    
     if (!inFile)
+    {
+        cout << "File could not open\n";
+        return 1;
+    }
+    else
+    {
+        inFile >> testcases;
+
+        while (testcases--)
+        {
+            int initial_length, extra_length;
+            string initial_word, extra_chars, assign;
+
+            inFile >> initial_length;   
+            inFile >> initial_word;   
+            inFile >> extra_length;   
+            inFile >> extra_chars;  
+            inFile >> assign;
+
+            // store result in a deque
+            deque<char> result(initial_word.begin(), initial_word.end());
+
+            // process each character
+            for (int i = 0; i < extra_length; i++)
+            {
+                if (assign[i] == 'V')
+                    // Vlad inserts at the front
+                    result.push_front(extra_chars[i]);
+                else 
+                    // Dima inserts at the back
+                    result.push_back(extra_chars[i]);
+            }
+            // convert deque back to string for output
+            string final_result(result.begin(), result.end());
+            cout << final_result << endl;
+        }
+    }
 
     return 0;
 }
